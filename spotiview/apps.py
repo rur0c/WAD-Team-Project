5bddb0a4f815f04eb1b8f8e22a6c3b6ac9907390
Bobi-V <gn.burito@gmail.com>
2023-02-27 19:12:55 +0000

    Basic Skeleton Structure for now

---
+++ spotiview/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpotiviewConfig(AppConfig):
+    name = 'spotiview'
---
+++ spotiview/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from spotiview import views
+
+app_name = 'spotiview'
+
+urlpatterns = [
+    
+]
